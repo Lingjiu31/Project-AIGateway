7e0e63dea752212cbd6cf3e5d816f71a14c253c9
LIngJiu31 <hzsun09@gmail.com>
2026-06-05 12:38:36 +0800

    fix: 修复 User.Username 索引字段缺少 size 标签

--- internal/user/model.go
+++ internal/user/model.go
@@ -4,8 +4,8 @@ import "time"
 
 type User struct {
 	ID        int64     `json:"id"         gorm:"primaryKey"`
-	Username  string    `json:"username"   gorm:"uniqueIndex"`
-	Password  string    `json:"-"`
+	Username  string    `json:"username"   gorm:"uniqueIndex;size:255"`
+	Password  string    `json:"-"          gorm:"size:255"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
